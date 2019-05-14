d0ce1529c8e11199aeca29eec6c4cceab264653f
dayaGuerra <dayamandalaman@gmail.com>
2019-05-13 21:38:21 -0500

    Inicializando Firebase

--- catalogo-alicorp/src/app/app.module.ts
+++ catalogo-alicorp/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// firebase
+import { AngularFireModule } from '@angular/fire'
+
+
+// import enviroment
+import { environment } from '../environments/environment'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- catalogo-alicorp/src/environments/environment.prod.ts
+++ catalogo-alicorp/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBEWR4ihYpFU2_7_rVMPJnflLoTrnDdqYo",
+    authDomain: "alicorp-catalogo-firestore.firebaseapp.com",
+    databaseURL: "https://alicorp-catalogo-firestore.firebaseio.com",
+    projectId: "alicorp-catalogo-firestore",
+    storageBucket: "alicorp-catalogo-firestore.appspot.com",
+    messagingSenderId: "76028865937",
+    appId: "1:76028865937:web:ec9bf396bd61a4cf"
+  }
 };
